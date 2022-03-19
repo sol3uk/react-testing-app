bf898cf2e11cdcb6fadd5335819ca022912baa41
Ben A <ben@sol3.me>
2022-03-19 17:01:27 +0000

    Add github pages deployment for testing

--- bens-app/src/components/Navigation.js
+++ bens-app/src/components/Navigation.js
@@ -8,10 +8,10 @@ const Navigation = () => {
                     <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                     <Navbar.Collapse id='responsive-navbar-nav'>
                         <Nav>
-                            <Nav.Link href='/'>Home</Nav.Link>
-                            <Nav.Link href='/find-game'>Find Game</Nav.Link>
-                            <Nav.Link href='/tic-tac-toe'>Tic Tac Toe</Nav.Link>
-                            <Nav.Link href='/counter'>Counter</Nav.Link>
+                            <Nav.Link to='/'>Home</Nav.Link>
+                            <Nav.Link to='/find-game'>Find Game</Nav.Link>
+                            <Nav.Link to='/tic-tac-toe'>Tic Tac Toe</Nav.Link>
+                            <Nav.Link to='/counter'>Counter</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
